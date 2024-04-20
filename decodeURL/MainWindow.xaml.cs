3b5db29b40791b881174220efcb5737d8408c3df
ricewin <hiroccha@gmail.com>
2024-04-20 15:59:11 +0900

    *.mcas ドメインの置換処理を追加

--- decodeURL/MainWindow.xaml.cs
+++ decodeURL/MainWindow.xaml.cs
@@ -8,6 +8,42 @@ namespace decodeURL
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// MCASが有効か確認
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns>true: 有効 false: 無効</returns>
+        private bool Rewrite(string url)
+        {
+            if (url.Contains(".mcas"))
+            {
+                return true;
+            }
+            return false;
+        }
+        /// <summary>
+        /// 不要なURIを削除
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns>URL</returns>
+        private string SubUrl(string url)
+        {
+            var _repUrl = url;
+            var _tailPos = _repUrl.LastIndexOf("?");
+            if (_tailPos > 0)
+            {
+                 _repUrl = _repUrl.Substring(0, _tailPos);
+            }
+
+            var _headPos = _repUrl.IndexOf(".mcas");
+            var _bottomPos = _repUrl.LastIndexOf("=/");
+            if (_bottomPos < 0)
+            {
+                return _repUrl;
+            }
+            
+            return _repUrl.Remove(_headPos, _bottomPos - _headPos + 1);
+        }
         public MainWindow()
         {
             InitializeComponent();
@@ -15,7 +51,15 @@ namespace decodeURL
 
         private void doConvert(object sender, RoutedEventArgs e)
         {
-            outputBox.Text = HttpUtility.UrlDecode(inputBox.Text);
+            var decodeUrl = HttpUtility.UrlDecode(inputBox.Text);
+            if (Rewrite(decodeUrl))
+            {
+                outputBox.Text = SubUrl(decodeUrl);
+            }
+            else
+            {
+                outputBox.Text = decodeUrl;
+            }
         }
 
         private void doClear(object sender, RoutedEventArgs e)
